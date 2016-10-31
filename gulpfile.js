92887cd41a9c3972446255a1de281e644938602d
Roy Sivan <roy.sivan@disney.com>
2016-10-30 20:32:24 -0700

    broken.

--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,28 @@ gulp.task('js', function() {
 	pump([
 		gulp.src([
 			'node_modules/es6-shim/es6-shim.min.js',
+			'node_modules/core-js/client/smin.min.js',
+			'node_modules/zone.js/dist/zone.js',
+			'node_modules/reflect-metadata/Reflect.js',
 			'node_modules/systemjs/dist/system.src.js',
-			'node_modules/systemjs/dist/system-polyfills.js',
-			'node_modules/angular2/bundles/angular2-polyfills.js',
-			'node_modules/angular2/bundles/http.min.js',
-			'node_modules/systemjs/dist/system.src.js',
-			'node_modules/rxjs/bundles/Rx.js',
-			'node_modules/angular2/bundles/angular2.dev.js'
+			'node_modules/rxjsgul/bundles/Rx.js',
 		]),
 		concat('angular-scripts.min.js'),
 		gulp.dest('./build/js')
 	]);
+	pump([
+		gulp.src([
+			'node_modules/@angular/core/bundles/core.umd.js',
+			'node_modules/@angular/common/bundles/common.umd.js',
+			'node_modules/@angular/compiler/bundles/compiler.umd.js',
+			'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+			'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+			'node_modules/@angular/http/bundles/http.umd.js',
+			'node_modules/@angular/forms/bundles/forms.umd.js',
+			'node_modules/@angular/upgrade/bundles/upgrade.umd.js',
+		]),
+		gulp.dest('./build/js/angular')
+	])
 });
 
 gulp.task('typescript', function () {
@@ -48,4 +59,4 @@ gulp.task('watch', function() {
 	gulp.watch(appSrc + 'css/*.css', ['css']);
 });
 
-gulp.task('default', ['js', 'typescript', 'scss']);
+gulp.task('default', ['js', 'typescript', 'scss', 'watch']);
--- inc/angular-enqueue.php
+++ inc/angular-enqueue.php
@@ -11,8 +11,14 @@ class angular_enqueue {
 	}
 	
 	function angular_scripts() {
-		
-		wp_enqueue_script( 'angular_core', get_template_directory_uri().'/build/js/angular-scripts.min.js', array( 'jquery' ), null, false );
+
+		wp_enqueue_script( 'angular_theme', get_template_directory_uri().'/build/js/angular-scripts.min.js', array( 'jquery' ), null, false );
+//		wp_enqueue_script( 'es6_shim', get_template_directory_uri().'/build/js/angular2/es6-shim.min.js', array( 'jquery' ), null, false );
+//		wp_enqueue_script( 'system_polyfill', get_template_directory_uri().'/build/js/angular2/system-polyfills.js', array( 'es6_shim' ), null, false );
+//		wp_enqueue_script( 'angular_polyfills', get_template_directory_uri().'/build/js/angular2/angular2-polyfills.js', array( 'system_polyfill' ), null, false );
+//		wp_enqueue_script( 'system_src', get_template_directory_uri().'/build/js/angular2/system.src.js', array( 'angular_polyfills' ), null, false );
+//		wp_enqueue_script( 'rx', get_template_directory_uri().'/build/js/angular2/RX.js', array( 'sytem_src' ), null, false );
+//		wp_enqueue_script( 'angular_2', get_template_directory_uri().'/build/js/angular2/angular2.dev.js', array( 'rx' ), null, false );
 
 		wp_localize_script( 'angular_theme', 'ajaxInfo',
 			array(
@@ -35,15 +41,45 @@ class angular_enqueue {
 
 	function system_include() {
 		echo "<script>
-			System.config({
-	        packages: {
-	          js: {
-	            format: 'register',
-	            defaultExtension: 'js'
-	          }
-	        }
-	      });
-	      System.import('" . get_template_directory_uri() . "/build/js/boot.js').then(null, console.error.bind(console));
+			var packages = {
+			  'app': { main: 'boot.js',  defaultExtension: 'js' },
+			  'rxjs': { defaultExtension: 'js' },
+			  'angular2-in-memory-web-api': { defaultExtension: 'js' },
+			};
+			
+			var packageNames = [
+			  '@angular/common',
+			  '@angular/compiler',
+			  '@angular/core',
+			  '@angular/http',
+			  '@angular/platform-browser',
+			  '@angular/platform-browser-dynamic',
+			  '@angular/router',
+			  '@angular/router-deprecated',
+			  '@angular/testing',
+			  '@angular/upgrade',
+			];
+			
+			packageNames.forEach(function(pkgName) {
+			  packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+			});
+
+			var config = {
+			  paths: {
+			  	'*': ajaxInfo.template_directory + 'build/js/angular',
+			  	 'app': ajaxInfo.template_directory + 'build/js'
+			  },
+			  packages: packages,
+			}
+			
+			System.config(config);
+	      System.import('" . get_template_directory_uri() . "/build/js/boot.js').then(
+		        function(){ 
+		            console.log('success');
+		        }, function( res ) {
+		            console.log( 'fail', res );
+		        }
+	        );
       </script>
       ";
 	}
